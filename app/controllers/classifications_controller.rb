6bc3eb4fd9d24730610bd2a418593416423e4ecc
YYZP524 <897116848@qq.com>
2016-12-15 10:09:26 +0800

    YYZP524

---
+++ app/controllers/classifications_controller.rb
@@ -0,0 +1,74 @@
+class ClassificationsController < ApplicationController
+
+  def index
+    @classifications = Classification.all
+
+  end
+
+  # GET /chapters/1
+  # GET /chapters/1.json
+  def show
+  end
+
+  # GET /chapters/new
+  def new
+    @classification  = Classification.new
+  end
+
+  # GET /chapters/1/edit
+  def edit
+
+  end
+
+  # POST /chapters
+  # POST /chapters.json
+  def create
+    @classification = Classification.new(classification_params)
+
+    respond_to do |format|
+      if @classification.save
+        format.html { redirect_to classifications_path, notice: 'Test was successfully created.' }
+        format.json { render :index, status: :created, location: @classification}
+      else
+        format.html { render :new }
+        format.json { render json: @classification.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /chapters/1
+  # PATCH/PUT /chapters/1.json
+  def update
+    respond_to do |format|
+      if @classification.update(classification_params)
+        format.html { redirect_to classifications_path, notice: 'Chapter was successfully updated.' }
+        format.json { render :show, status: :ok, location: @classification }
+      else
+        format.html { render :edit }
+        format.json { render json: @classification.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /chapters/1
+  # DELETE /chapters/1.json
+  def destroy
+    @classification.destroy
+    respond_to do |format|
+      format.html { redirect_to classifications_url, notice: 'Chapter was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_classification
+    @classification = Classification.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def classification_params
+    params.require(:classification ).permit(:classname )
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   end
 
   resources :users
-
+  resources :classifications
 
 
 end
